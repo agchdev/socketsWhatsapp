0adfb6cba5f426a49c2053c61abef100eb5b4645
agchdev <alex.chinchi02@gmail.com>
2024-11-25 23:46:26 +0100

    whatsapp

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const server = http.createServer(app)
 const {Server} = require('socket.io')
 const io = new Server(server)
 
+
 io.on('connection', (socket) => {
     console.log('Un usuario se ha conectado')
 
